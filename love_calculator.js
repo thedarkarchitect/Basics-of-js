1ae9622bea0d271dddc306dd3bce060b1bf3ac37
thedarkarchitect <romanreynolds240@gmail.com>
2023-03-05 11:27:20 +0300

    second commit

---
+++ love_calculator.js
@@ -0,0 +1,5 @@
+var name1 = prompt("Enter your name?");
+var name2 = prompt("Enter your name?");
+var loveScore = Math.random();
+loveScore = Math.floor(loveScore * 100) + 1;
+alert("Your compatibility is "+ loveScore+ "%");
