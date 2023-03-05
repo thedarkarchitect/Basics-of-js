1c2a77b74aba626b3e730f554c65159a50b16356
thedarkarchitect <romanreynolds240@gmail.com>
2023-03-05 12:05:16 +0300

    third commit

--- love_calculator.js
+++ love_calculator.js
@@ -1,5 +1,14 @@
 var name1 = prompt("Enter your name?");
 var name2 = prompt("Enter your name?");
+
 var loveScore = Math.random();
 loveScore = Math.floor(loveScore * 100) + 1;
-alert("Your compatibility is "+ loveScore+ "%");
+
+if(loveScore === 100){
+    alert("Your compatibility is "+ loveScore+ "% You love eachother like kanye loves kanye.");
+    
+}else if(loveScore > 50){
+    alert("Your compatibility is "+ loveScore+ "% You go well together.");
+}else{
+    alert("Your compatibility is "+ loveScore+ "% its a far shot for you to blossom");
+}
