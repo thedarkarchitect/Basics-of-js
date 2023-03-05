c175f8f526ad6b9614f8ab8587b0800fcdc04845
thedarkarchitect <romanreynolds240@gmail.com>
2023-03-05 13:16:35 +0300

    final commit

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,21 +2,24 @@ var output = [];
 var num = 1;
 
 function fizzBuzz(){
-    
-    if (num % 5 === 0 && num % 3 === 0){
-        output.push("FizzBuzz");
-    }else if (num % 5 === 0){
-        output.push("Buzz");
-    }else if (num % 3 === 0){
-        output.push("Fizz");
-    }else{
-        output.push(num);
-    }
+    while(num <= 100){
+        if (num % 5 === 0 && num % 3 === 0){
+            output.push("FizzBuzz");
+        }else if (num % 5 === 0){
+            output.push("Buzz");
+        }else if (num % 3 === 0){
+            output.push("Fizz");
+        }else{
+            output.push(num);
+        }
 
-    num ++;
+        num ++;
+    }
+    
     console.log(output);
     
 }
 
 fizzBuzz();//this will only show one was added but in the developer options of google the last number is remembered so an increment will happen and other numbers will be added
 // to make this work you'd need a for loop 
+
